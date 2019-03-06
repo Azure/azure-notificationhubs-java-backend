fdb9d4dda91d19e90dd3a885cb70665949e3978d
Maksim Lebedev <alessarik@gmail.com>
2019-03-06 17:57:25 +0300

    Spaces...

--- NotificationHubs/src/com/windowsazure/messaging/HttpClientManager.java
+++ NotificationHubs/src/com/windowsazure/messaging/HttpClientManager.java
@@ -6,61 +6,61 @@ import org.apache.http.client.config.RequestConfig;
 
 public class HttpClientManager {
 
-	private static CloseableHttpAsyncClient httpAsyncClient;
+    private static CloseableHttpAsyncClient httpAsyncClient;
 
-	// A timeout value of zero is interpreted as an infinite timeout.
-	// A negative value is interpreted as undefined (system default).
+    // A timeout value of zero is interpreted as an infinite timeout.
+    // A negative value is interpreted as undefined (system default).
 
-	// The timeout in milliseconds used when requesting a connection from the connection manager.
-	private static int ConnectionRequestTimeout = -1;
+    // The timeout in milliseconds used when requesting a connection from the connection manager.
+    private static int ConnectionRequestTimeout = -1;
 
-	// The timeout in milliseconds until a connection is established.
-	private static int ConnectionTimeout = -1;
+    // The timeout in milliseconds until a connection is established.
+    private static int ConnectionTimeout = -1;
 
-	// The socket timeout in milliseconds, which is the timeout for waiting for data or,
-	// put differently, a maximum period inactivity between two consecutive data packets.
-	private static int SocketTimeout = -1;
+    // The socket timeout in milliseconds, which is the timeout for waiting for data or,
+    // put differently, a maximum period inactivity between two consecutive data packets.
+    private static int SocketTimeout = -1;
 
-	public static CloseableHttpAsyncClient getHttpAsyncClient() {
-		if (httpAsyncClient == null) {
-			synchronized(HttpClientManager.class) {
-				if (httpAsyncClient == null) {
-					RequestConfig config = RequestConfig.custom()
-														.setConnectionRequestTimeout(ConnectionRequestTimeout)
-														.setConnectTimeout(ConnectionTimeout)
-														.setSocketTimeout(SocketTimeout)
-														.build();
-					CloseableHttpAsyncClient client = HttpAsyncClientBuilder.create()
-																			.setDefaultRequestConfig(config)
-																			.build();
-					client.start();
-					httpAsyncClient = client;
-				}
-			}
-		}
-		return httpAsyncClient;
-	}
+    public static CloseableHttpAsyncClient getHttpAsyncClient() {
+        if (httpAsyncClient == null) {
+            synchronized (HttpClientManager.class) {
+                if (httpAsyncClient == null) {
+                    RequestConfig config = RequestConfig.custom()
+                            .setConnectionRequestTimeout(ConnectionRequestTimeout)
+                            .setConnectTimeout(ConnectionTimeout)
+                            .setSocketTimeout(SocketTimeout)
+                            .build();
+                    CloseableHttpAsyncClient client = HttpAsyncClientBuilder.create()
+                            .setDefaultRequestConfig(config)
+                            .build();
+                    client.start();
+                    httpAsyncClient = client;
+                }
+            }
+        }
+        return httpAsyncClient;
+    }
 
-	public static void setHttpAsyncClient(CloseableHttpAsyncClient httpAsyncClient) {
-		synchronized(HttpClientManager.class) {
-			if (HttpClientManager.httpAsyncClient == null) {
-				HttpClientManager.httpAsyncClient = httpAsyncClient;
-			}
-			else {
-				throw new RuntimeException("HttpAsyncClient was already set before or default one is being used.");
-			}
-		}
-	}
+    public static void setHttpAsyncClient(CloseableHttpAsyncClient httpAsyncClient) {
+        synchronized (HttpClientManager.class) {
+            if (HttpClientManager.httpAsyncClient == null) {
+                HttpClientManager.httpAsyncClient = httpAsyncClient;
+            }
+            else {
+                throw new RuntimeException("HttpAsyncClient was already set before or default one is being used.");
+            }
+        }
+    }
 
-	public static void setConnectionRequestTimeout(int timeout) {
-		ConnectionRequestTimeout = timeout;
-	}
+    public static void setConnectionRequestTimeout(int timeout) {
+        ConnectionRequestTimeout = timeout;
+    }
 
-	public static void setConnectTimeout(int timeout) {
-		ConnectionTimeout = timeout;
-	}
+    public static void setConnectTimeout(int timeout) {
+        ConnectionTimeout = timeout;
+    }
 
-	public static void setSocketTimeout(int timeout) {
-		SocketTimeout = timeout;
-	}
+    public static void setSocketTimeout(int timeout) {
+        SocketTimeout = timeout;
+    }
 }
